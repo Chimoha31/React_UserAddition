495aeae0cbfe5ffbb6eccc59dea764be37d98afa
Chimoha31 <mokochii1108@gmail.com>
2022-01-25 23:25:20 -0800

    Create UserList.files and have to fix Error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import AddUser from './components/Users/AddUser';
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
+const App = () => {
+  const [usersList, setUsersList] = useState("");
 
-function App() {
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
-   <AddUser />
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={[usersList]} />
+    </div>
   );
 }
 
