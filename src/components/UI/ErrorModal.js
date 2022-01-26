d9e77592a75d111a61b9921c8bc76065740510de
Chimoha31 <mokochii1108@gmail.com>
2022-01-25 23:28:12 -0800

    Create ErrorModal

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from '../UI/ErrorModal.module.css';
+
+const ErrorModal = (props) => {
+
+  return(
+    <div>
+
+    </div>
+  );
+};
+
+export default ErrorModal;
